38ae32e778b4978bb9fc52d590fb2cf2227e6e6e
Elan van Biljon <elan@vanbiljon.co.za>
2018-03-21 03:27:12 +0200

    Change sound to play asynchronously by default

--- pomodoro.py
+++ pomodoro.py
@@ -18,46 +18,58 @@ class TaskInfoWriter():
 
 class SoundPlayer():
 	def __init__(self, sound_file):
-		#define stream chunk
-		self.chunk = 1024
-
-		#open a wav format music
+		# open a wav format sound file
 		self.sound_file = wave.open(sound_file, "rb")
-		#instantiate PyAudio
+
+		# instantiate PyAudio
 		self.audio_player = pyaudio.PyAudio()
 
-		#open stream
+		# open stream
 		self.stream = self.audio_player.open(
 			format = self.audio_player.get_format_from_width(
 				self.sound_file.getsampwidth()
 			),
 			channels = self.sound_file.getnchannels(),
 			rate = self.sound_file.getframerate(),
-			output = True
+			output = True,
+			stream_callback=self.get_next_frame
 		)
 
-	def play_sound(self):
-		#read data
-		data = self.sound_file.readframes(self.chunk)
+	def get_next_frame(self, in_data, frame_count, time_info, status):
+		# get the next chunk of sound data
+		data = self.sound_file.readframes(frame_count)
+		return (data, pyaudio.paContinue)
 
-		#play stream
-		while data:
-			self.stream.write(data)
-			data = self.sound_file.readframes(self.chunk)
 
+	def play_sound(self, blocking=False):
+		# close stream if it was previously open
+		s.stream.stop_stream()
+
+		# start reading sound file from the beginning
 		self.sound_file.rewind()
 
+		# start stream (this starts the sound playing)
+		self.stream.start_stream()
+
+		# if blocking is False, this will play asynchronously
+		while blocking and s.stream.is_active():
+			time.sleep(0.1)
+
 	def close(self):
-		#stop stream
+		# stop stream
 		self.stream.stop_stream()
 		self.stream.close()
 
-		#close PyAudio
+		# close wave file
+		self.sound_file.close()
+
+		# close PyAudio
 		self.audio_player.terminate()
 
 if __name__ == "__main__":
 	s = SoundPlayer("./time.wav")
 	TaskInfoWriter.start_task(" ".join(sys.argv[1:]))
 
-	s.play_sound()
+	s.play_sound(blocking=True)
+
 	s.close()
