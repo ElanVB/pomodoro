e1a5c428bf3eebcdc9c8c486add5d0d445df0857
Elan van Biljon <elan@vanbiljon.co.za>
2018-03-23 11:50:32 +0200

    Add different time for notes and add break

--- pomodoro.py
+++ pomodoro.py
@@ -48,17 +48,17 @@ class TaskInfoWriter():
 	def __init__(self):
 		pass
 
-	def start_task(self, name=None):
+	def start_task(self, name=None, logs_dir="./logs"):
 		if name == None:
 			raise Exception("You must give the task a name.")
 
 		dir_name = time.strftime("%b_%Y")
 
-		file_name = time.strftime("%a %d %b %Y")
+		file_name = time.strftime("%d_(%a)_%b_%Y")
 
 		current_time = time.strftime("# %H:%M - {}".format(name))
 
-		logs_path = Path("./logs")
+		logs_path = Path(logs_dir)
 
 		if not logs_path.is_dir():
 			os.mkdir(logs_path)
@@ -105,7 +105,7 @@ class SoundPlayer():
 
 	def play_sound(self, blocking=False):
 		# close stream if it was previously open
-		s.stream.stop_stream()
+		self.stream.stop_stream()
 
 		# start reading sound file from the beginning
 		self.sound_file.rewind()
@@ -114,7 +114,7 @@ class SoundPlayer():
 		self.stream.start_stream()
 
 		# if blocking is False, this will play asynchronously
-		while blocking and s.stream.is_active():
+		while blocking and self.stream.is_active():
 			time.sleep(0.1)
 
 	def close(self):
@@ -129,34 +129,49 @@ class SoundPlayer():
 		self.audio_player.terminate()
 
 if __name__ == "__main__":
-	s = SoundPlayer("./time.wav")
+	sound = SoundPlayer("./time.wav")
 
 	print("===================================================================")
 	title = input("What is the name of this task?\n")
 	writer = TaskInfoWriter()
+	writer.start_task(title)
 
 	string_time = input(
 		"How long do you want to spend on this task?\n[Hours:Minutes:seconds]\n"
 	)
-	array_time = list(map(float, string_time.split(":"))) # this still needs proper error handling
+	try:
+		array_time = list(map(float, string_time.split(":")))
+	except:
+		print("Error with time string, defaulting to 25 mins.")
+		array_time = [0, 25, 0]
 	dict_time = {
 		"hours": array_time[0],
 		"minutes": array_time[1],
 		"seconds": array_time[2]
 	}
-	t = TaskTimer(dict_time)
-	# t = TaskTimer(dict_time, lambda: False)
+	task = TaskTimer(dict_time)
 
-	while not t.is_done():
+	while not task.is_done():
 		time.sleep(0.1)
 
 	# TaskInfoWriter.start_task(" ".join(sys.argv[1:]))
 
-	s.play_sound(blocking=True)
+	sound.play_sound()
 
-	writer.start_task(title)
-
-	n = TaskTimer(dict_time, s.play_sound)
+	string_time = input(
+		"How long do you want to spend on writing notes?\n[Hours:Minutes:seconds]\n"
+	)
+	try:
+		array_time = list(map(float, string_time.split(":")))
+	except:
+		print("Error with time string, defaulting to 5 mins.")
+		array_time = [0, 5, 0]
+	dict_time = {
+		"hours": array_time[0],
+		"minutes": array_time[1],
+		"seconds": array_time[2]
+	}
+	note = TaskTimer(dict_time, sound.play_sound)
 	notes_input_message = (
 """
 What notes do you have on the task you have just completed?
@@ -174,4 +189,23 @@ What notes do you have on the task you have just completed?
 
 	writer.write_notes("\n".join(notes)) # if you enter notes before callback, error occours because stream is closed
 
-	s.close()
+	string_time = input(
+		"How long of a break do you want to take?\n[Hours:Minutes:seconds]\n"
+	)
+	try:
+		array_time = list(map(float, string_time.split(":")))
+	except:
+		print("Error with time string, defaulting to 5 mins.")
+		array_time = [0, 5, 0]
+	dict_time = {
+		"hours": array_time[0],
+		"minutes": array_time[1],
+		"seconds": array_time[2]
+	}
+	break_task = TaskTimer(dict_time)
+
+	while not break_task.is_done():
+		time.sleep(0.1)
+	sound.play_sound(blocking=True)
+
+	sound.close()
